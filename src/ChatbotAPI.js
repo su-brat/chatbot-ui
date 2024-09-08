1f4f2766f7c708121b1db6f002d0af14a2657f88
Subrat Kumar Pattanaik <subratpatnaik.email@gmail.com>
2024-09-09 01:57:57 +0530

    feat: chatbot API based answer

--- src/ChatbotAPI.js
+++ src/ChatbotAPI.js
@@ -1,12 +1,21 @@
+import { CHATBOT_API_URL } from "./constants";
+
 const API = {
-  GetChatbotResponse: async message => {
-    return new Promise(function(resolve, reject) {
-      setTimeout(function() {
-        if (message === "hi") resolve("Welcome to chat!");
-        else resolve("echo : " + message);
-      }, 2000);
-    });
-  }
+  GetChatbotResponse: async (message) => {
+    try {
+      if (message.trim().toLowerCase() == 'hi') {
+        return "Hello! How can I help you?";
+      }
+      const response = await fetch(
+        `${CHATBOT_API_URL}/ask?question="${message}"`
+      );
+      const conversation = await response.json();
+      return conversation["answer"] ?? "Sorry, I didn't understand";
+    } catch (err) {
+      console.error(err);
+      return "Sorry, I am currently unavailable";
+    }
+  },
 };
 
 export default API;
