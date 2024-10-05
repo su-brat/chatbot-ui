69572546964d163bc84ee08828e64b77378599d4
Subrat Kumar Pattanaik <subratpatnaik.email@gmail.com>
2024-10-05 22:07:44 +0530

    refactor: rewritten chatbot call and response handling (#13)

--- src/ChatbotAPI.js
+++ src/ChatbotAPI.js
@@ -1,6 +1,23 @@
-import { CHATBOT_API_URL } from "./constants";
+import { CHATBOT_API_URL, RESPONSE_CONFIDENCE_THRESHOLD } from "./constants";
 
 const greetMessages = ["hi", "hello"];
+
+const parseAPIResponse = async (response) => {
+  const data = await response.json();
+  if (response.ok) {
+    const answers = data["answers"];
+    const validAnswers = answers.filter((answer) => answer["answer"].trim() !== "");
+    console.log(validAnswers);
+    if (validAnswers.length > 0 && validAnswers[0]["score"] > RESPONSE_CONFIDENCE_THRESHOLD) {
+      return validAnswers[0]["answer"];
+    } else {
+      return "Sorry, I either did not understand or cannot answer that";
+    }
+  } else {
+    console.error(data);
+    return "Sorry, I am currently unavailable";
+  }
+}
 
 const API = {
   GetChatbotResponse: async (message) => {
@@ -12,10 +29,19 @@ const API = {
       ) {
         return "Hello! How can I help you?";
       }
-      const endpoint = `${CHATBOT_API_URL}/ask?question=${message}&fallback=llm`;
-      const response = await fetch(endpoint);
-      const conversation = await response.json();
-      return conversation["answer"] ?? "Sorry, I didn't understand";
+      const endpoint = `${CHATBOT_API_URL}/query`;
+      const response = await fetch(endpoint, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          query: message,
+          params: {},
+          debug: false,
+        }),
+      });
+      return parseAPIResponse(response);
     } catch (err) {
       console.error(err);
       return "Sorry, I am currently unavailable";
